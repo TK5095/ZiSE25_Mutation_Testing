a4314a702b9d3375151add659ed30f64f828679f
Nicola Vetrini <nicola.vetrini@bugseng.com>
2025-09-21 13:30:26 +0200

    Split tests for twister and link requirements via comments.

--- tests/test_temp_alert/test_buzzer.c
+++ tests/test_temp_alert/test_buzzer.c
@@ -1,6 +1,9 @@
 #include <zephyr/ztest.h>
 #include "buzzer.h"
 
+/**
+ * @tests SRS-04
+ */
 ZTEST(buzzer_tests, test_buzzer_fire_pattern_runs)
 {
     /* We expect no crash or error, can't assert much in unit test */
--- tests/test_temp_alert/test_display.c
+++ tests/test_temp_alert/test_display.c
@@ -7,6 +7,9 @@ ZTEST(display_tests, test_display_init_success)
     zassert_equal(ret, 0, "display_init() should return 0 on success, got %d", ret);
 }
 
+/**
+ * @tests SRS-02
+ */
 ZTEST(display_tests, test_display_write_zeroes)
 {
     uint8_t zeroes[4] = {0, 0, 0, 0};
@@ -14,14 +17,17 @@ ZTEST(display_tests, test_display_write_zeroes)
     zassert_equal(ret, 0, "display_write() failed with zeroes, got %d", ret);
 }
 
+/**
+ * @tests SRS-02
+ */
 ZTEST(display_tests, test_display_write_valid_digits)
 {
     extern const uint8_t tm1637_segment_map[16];
     uint8_t digits[4] = {
-        tm1637_segment_map[1],
-        tm1637_segment_map[2],
-        tm1637_segment_map[3],
-        tm1637_segment_map[4]
+        tm1637_segment_map[1], /* digit '1' */
+        tm1637_segment_map[2], /* digit '2' */
+        tm1637_segment_map[3], /* digit '3' */
+        tm1637_segment_map[4]  /* digit '4' */
     };
     int ret = display_write(digits);
     zassert_equal(ret, 0, "display_write() failed with valid digits, got %d", ret);
